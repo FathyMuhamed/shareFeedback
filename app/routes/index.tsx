12ac8706f8f2a5a53f6ef68bb8fd6c2299981045
FathyMuhamed <fathimuhamed10522@gmail.com>
2022-05-10 21:54:36 +0200

    update package.json

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -8,8 +8,8 @@ export const loader: LoaderFunction = async ({ request }: any) => {
 export default function home() {
   return (
     <div className='flex items-center justify-center h-screen '>
-      <h1 className='text-2xl font-bold text-red-800'>
-        Welcome to Remix + Prisma.
+      <h1 className='text-2xl font-bold text-blue-800'>
+        Welcome to Remix + Prisma
       </h1>
     </div>
   );
